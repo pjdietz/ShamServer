d4bd8bc7737bf4d1374e4eec50a1f10583b69eb7
66d27b3ea53eda58ad7f71253dc2338b08efe4c4 <botrelli@gmx.com>
2014-08-02 18:33:07 -0400

    CS Fixes

--- src/pjdietz/ShamServer/StringShamServer.php
+++ src/pjdietz/ShamServer/StringShamServer.php
@@ -9,10 +9,10 @@ class StringShamServer extends ShamServer
      *
      * Pass an entire PHP file as a string for the $router parameter.
      *
-     * @param string $host Hostname to listen for
-     * @param int $port Port to list on
-     * @param string $router PHP script as a string
-     * @param int $timeout Time in microseconds to wait for the webserver to start
+     * @param string $host    Hostname to listen for
+     * @param int    $port    Port to list on
+     * @param string $router  PHP script as a string
+     * @param int    $timeout Time in microseconds to wait for the webserver to start
      * @internal param string $script Path to PHP router file
      */
     public function __construct($host, $port, $router, $timeout = 5000)
